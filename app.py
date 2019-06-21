7585971b483f879a715a68b7023941bc5fd1fa5a
thejuly <dcsbailey21574@gmail.com>
2019-06-21 22:38:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -61,8 +61,8 @@ app = Flask(__name__)
 #line_bot_api = LineBotApi('wbeBaLPb7xIuGymdaHU9yHy300QZ383XYgewhXLSoRe3TnlWo1xQuypNFpis1ExGrSTV1WpmtmQEiaR9tRPQHFUspwI9rVk2Ajfrg1WUwFpV9ewvq/vDx9LItfeNW+9y6Ih/OcwNpJPB/UfE9afIFwdB04t89/1O/w1cDnyilFU=') # Channel Access Token DCS-BOT
 #handler = WebhookHandler('49a9d31e3b8135ee7f85e6bc78848baa') # Channel Secret DCS-BOT
 
-line_bot_api = LineBotApi('CFaQZEnotjzhRjT4JMbcMyA0cksM4HE9Zu7HA8xQdWVbOMh6HLdeiCp2NKv6gmjNmJHs+NhWvnk8DYUZ3r8mCX6goDgYby/EV9s+/2mR9piHnPUVUGzWhRlmWw5K8RQG8iwgtSPnw89zEaUILnhtMwdB04t89/1O/w1cDnyilFU=') # Channel Access Token Report
-handler = WebhookHandler('f1e03e9bb185204d4494a1cce993970a') # Channel Secret Report
+line_bot_api = LineBotApi('yl/DGv8veb6N9/DFulA+m7AA8zi7EpISACJfaTEQXix8hZinhLKu9JQOrrGU0MjmD2V4VcB51qZUKjhIANDCQga0bj1L/eWL4fmBbTbqMYOPsZAyEaho/EgLAb17zDr7JnUNUVS0v2Jl6vw5pZasVQdB04t89/1O/w1cDnyilFU=') # Channel Access Token Report
+handler = WebhookHandler('1c3794a985832d0671f1294545cb9bbb') # Channel Secret Report
 #end Token
 
 # for test route
